e71b6828da5c8ba2ce8d057ee058e7f5fbdfeab9
Ahmed-Techie <sudheer.ai@yahoo.com>
2023-03-18 08:46:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Card from "./components/card";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="pricing py-5">
+        <div className="container">
+          <div className="row">
+            <Card price="40" />
+            <Card price="50" />
+            <Card price="60" />
+            <Card price="70" />
+            <Card price="80" />
+            <Card price="90" />
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
